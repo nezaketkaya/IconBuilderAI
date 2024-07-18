fab7197adf6f5c6173734c388de29edea2a646d9
nezaketkaya <kayanezak@hotmail.com>
2024-07-18 21:33:39 +0300

    CQRS

--- IconBuilderAI.Infrastructure/DependencyInjection.cs
+++ IconBuilderAI.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,7 @@
 ﻿using IconBuilderAI.Application.Common.Interfaces;
 using IconBuilderAI.Infrastructure.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -13,12 +14,12 @@ namespace IconBuilderAI.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddScoped<IApplicationDbContext, ApplicationDbContext>
                               (container => container.GetRequiredService<ApplicationDbContext>());
 
-            services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(""));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
 
             return services;
         }
