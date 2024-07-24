8ccb145ad25b68a16408994427e47f3c7c25d07d
nezaketkaya <kayanezak@hotmail.com>
2024-07-24 23:47:59 +0300

    OrderDeleteCommand added

---
+++ IconBuilderAI.Application/Features/Orders/Commands/Delete/OrderDeleteCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IconBuilderAI.Application.Features.Orders.Commands.Delete
+{
+    public class OrderDeleteCommand : IRequest<Guid>
+    {
+        public Guid Id { get; set; }
+
+        public OrderDeleteCommand(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
