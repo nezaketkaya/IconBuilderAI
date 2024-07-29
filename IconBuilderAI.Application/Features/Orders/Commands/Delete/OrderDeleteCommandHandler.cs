d0672a2ad77e644dfae590c871a6c5d3f54b5742
nezaketkaya <kayanezak@hotmail.com>
2024-07-29 23:55:49 +0300

    OrderController added

--- IconBuilderAI.Application/Features/Orders/Commands/Delete/OrderDeleteCommandHandler.cs
+++ IconBuilderAI.Application/Features/Orders/Commands/Delete/OrderDeleteCommandHandler.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using IconBuilderAI.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,23 @@ using System.Threading.Tasks;
 
 namespace IconBuilderAI.Application.Features.Orders.Commands.Delete
 {
-    internal class OrderDeleteCommandHandler
+    public class OrderDeleteCommandHandler : IRequestHandler<OrderDeleteCommand, Guid>
     {
+        private readonly IApplicationDbContext _dbContext;
+
+        public OrderDeleteCommandHandler(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Guid> Handle(OrderDeleteCommand request, CancellationToken cancellationToken)
+        {
+            var order = await _dbContext.Orders.FirstOrDefaultAsync(x  => x.Id == request.Id, cancellationToken);
+           
+            _dbContext.Orders.Remove(order);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return order.Id;
+        }
     }
 }
---
+++ IconBuilderAI.WebApi/Controllers/OrdersController.cs
@@ -0,0 +1,25 @@
+﻿using IconBuilderAI.Application.Features.Orders.Commands.Delete;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IconBuilderAI.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : Controller
+    {
+        private readonly ISender _mediatr;
+
+        public OrdersController(ISender mediatr)
+        {
+            _mediatr = mediatr;
+        }
+
+
+        [HttpDelete("{id:guid")]
+        public async Task<IActionResult> DeleteAsync(Guid id, CancellationToken cancellationToken)
+        {
+            return Ok(await _mediatr.Send(new OrderDeleteCommand(id), cancellationToken));
+        }
+    }
+}
