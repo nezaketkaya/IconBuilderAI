baa0b9108c7f3787159d2a8dc3789f29a58b65bc
nezaketkaya <kayanezak@hotmail.com>
2024-08-21 02:30:17 +0300

    JwtManager added

---
+++ IconBuilderAI.Infrastructure/Services/JwtManager.cs
@@ -0,0 +1,16 @@
+﻿using IconBuilderAI.Application.Common.Interfaces;
+using IconBuilderAI.Domain.Settings;
+using Microsoft.Extensions.Options;
+
+namespace IconBuilderAI.Infrastructure.Services
+{
+    public class JwtManager : IJwtService
+    {
+        private readonly JwtSettings _jwtSettings;
+
+        public JwtManager(IOptions<JwtSettings> jwtSettingsOptions)
+        {
+            _jwtSettings = jwtSettingsOptions.Value;
+        }
+    }
+}
