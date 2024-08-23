f0b11a164eed7a78337c121454043c5bdbe20d05
nezaketkaya <kayanezak@hotmail.com>
2024-08-23 22:24:05 +0300

    Logging added

--- IconBuilderAI.WebApi/Program.cs
+++ IconBuilderAI.WebApi/Program.cs
@@ -5,46 +5,63 @@ using IconBuilderAI.WebApi;
 using IconBuilderAI.WebApi.Filters;
 using IconBuilderAI.WebApi.Services;
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("C:\\Users\\ali\\Desktop\\Logs\\log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
-// Add services to the container.
-
-builder.Services.AddControllers(opt =>
+try
 {
-    opt.Filters.Add<GlobalExceptionFilter>();
-});
+    Log.Information("Starting web application");
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Services.AddSerilog();
 
-builder.Services.AddHttpContextAccessor();
+    builder.Services.AddControllers(opt =>
+    {
+        opt.Filters.Add<GlobalExceptionFilter>();
+    });
 
-builder.Services.AddScoped<ICurrentUserService, CurrentUserManager>();
-builder.Services.AddApplication();
-builder.Services.AddInfrastructure(builder.Configuration);
-builder.Services.AddWebServices(builder.Configuration);
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-builder.Services.Configure<ApiBehaviorOptions>(options =>
-{
-    options.SuppressModelStateInvalidFilter = true;
+    builder.Services.AddHttpContextAccessor();
+
+    builder.Services.AddScoped<ICurrentUserService, CurrentUserManager>();
+    builder.Services.AddApplication();
+    builder.Services.AddInfrastructure(builder.Configuration);
+    builder.Services.AddWebServices(builder.Configuration);
+
+    builder.Services.Configure<ApiBehaviorOptions>(options =>
+    {
+        options.SuppressModelStateInvalidFilter = true;
+    }
+    );
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
 }
-);
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+catch(Exception e)
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Fatal(e, "Application terminated unexpectedly");
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+finally
+{
+    Log.CloseAndFlush();
+}
