60949c647637f149b1d36e85ea193f44df734e4d
nezaketkaya <kayanezak@hotmail.com>
2024-09-08 16:46:20 +0300

    parameter errors fixed

--- IconBuilderAI.WebApi/Controllers/OrdersController.cs
+++ IconBuilderAI.WebApi/Controllers/OrdersController.cs
@@ -1,14 +1,17 @@
 ﻿using IconBuilderAI.Application.Features.Orders.Commands.Add;
 using IconBuilderAI.Application.Features.Orders.Commands.Delete;
+using IconBuilderAI.Application.Features.Orders.Queries.GetAll;
 using IconBuilderAI.Application.Features.Orders.Queries.GetById;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IconBuilderAI.WebApi.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
-    public class OrdersController : Controller
+    public class OrdersController : ControllerBase
     {
         private readonly ISender _mediatr;
 
@@ -17,23 +20,29 @@ namespace IconBuilderAI.WebApi.Controllers
             _mediatr = mediatr;
         }
 
-        [HttpPost]
-        public async Task<IActionResult> AddAsync(OrderAddCommand command, CancellationToken cancellationToken)
-        {
-            return Ok(await _mediatr.Send(command, cancellationToken)); 
-        }
-
         [HttpGet("{id:guid}")]
         public async Task<IActionResult> GetByIdAsync(Guid id, CancellationToken cancellationToken)
         {
             return Ok(await _mediatr.Send(new OrderGetByIdQuery(id), cancellationToken));
         }
 
-
         [HttpDelete("{id:guid}")]
         public async Task<IActionResult> DeleteAsync(Guid id, CancellationToken cancellationToken)
         {
             return Ok(await _mediatr.Send(new OrderDeleteCommand(id), cancellationToken));
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddAsync(OrderAddCommand command, CancellationToken cancellationToken)
+        {
+            return Ok(await _mediatr.Send(command, cancellationToken));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync(CancellationToken cancellationToken)
+        {
+            return Ok(await _mediatr.Send(new OrderGetAllQuery(), cancellationToken));
         }
     }
 }
